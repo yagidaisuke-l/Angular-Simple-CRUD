03f6dacf7f496459896ea64fbc3bbc05d299ff7d
yagi_d <yagi.d@inlife.work>
2024-07-08 00:30:47 +0900

    ブログタイトルを一覧ページに表示仮実装

--- angular-simple-crud/src/app/blog/blog-list/blog-list.component.ts
+++ angular-simple-crud/src/app/blog/blog-list/blog-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.scss'
 })
 export class BlogListComponent {
-
+  blogs = [
+    'テストタイトル1', 'テストタイトル2'
+  ]
+  constructor() {}
 }
